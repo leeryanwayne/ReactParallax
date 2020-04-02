77a7df0b848c3762a61b9669247319def34b6aa6
leeryanwayne <59073161+leeryanwayne@users.noreply.github.com>
2020-04-02 15:10:52 -0600

    Added Parallax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+import Parallax from "./parallax.js";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div
+      className="App"
+      style={{ background: "grey", width: "100%", height: "100%" }}
+    >
+      <Parallax>
+        <div style={{ background: "red", width: "50px", height: "50px" }} />
+        <div style={{ background: "blue", width: "50px", height: "50px" }} />
+        <div style={{ background: "green", width: "50px", height: "50px" }} />
+      </Parallax>
     </div>
   );
 }
---
+++ src/parallax.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./styles.css";
+
+class Parallax extends React.Component {
+  static defaultProps = {
+    children: [],
+    intensity: 200,
+    mouseListenerZIndex: 5000
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mouseX: 0,
+      mouseY: 0
+    };
+
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+
+    this.id = "parallax-" + Math.random() * 1000;
+  }
+
+  componentDidUpdate() {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+  }
+
+  onMouseMove = e => {
+    var m_posx = 0,
+      m_posy = 0,
+      e_posx = 0,
+      e_posy = 0,
+      obj = this;
+    //get mouse position on document crossbrowser
+    if (!e) {
+      e = window.event;
+    }
+    if (e.pageX || e.pageY) {
+      m_posx = e.pageX;
+      m_posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+      m_posx =
+        e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+      m_posy =
+        e.clientY +
+        document.body.scrollTop +
+        document.documentElement.scrollTop;
+    }
+    //get parent element position in document
+    if (obj.offsetParent) {
+      /*
+      do{ 
+          e_posx += obj.offsetLeft;
+          e_posy += obj.offsetTop;
+      } while (obj = obj.offsetParent);
+      */
+    }
+
+    this.setState({ mouseX: m_posx - e_posx, mouseY: m_posy - e_posy });
+  };
+
+  render() {
+    let mouseX = this.props.mouseX || this.state.mouseX;
+    let mouseY = this.props.mouseY || this.state.mouseY;
+
+    let offsetX = mouseX - this.innerWidth / 2;
+    let offsetY = mouseY - this.innerHeight / 2;
+
+    return (
+      <div id={this.id} className="paralax">
+        {!this.props.mouseX && !this.props.mouseY && (
+          <div
+            style={{
+              position: "fixed",
+              width: "inherit",
+              height: "inherit",
+              zIndex: this.props.mouseListenerZIndex
+            }}
+            onMouseMove={this.onMouseMove}
+          />
+        )}
+        <div style={{ width: "inherit", height: "inherit" }}>
+          {this.props.children.map((child, index) => {
+            // TODO - Reverse intensity value
+            let offset = 8 - index / (this.props.intensity / 100);
+
+            return (
+              <div
+                className="centered"
+                style={{
+                  zIndex: index,
+                  width: "inherit",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  position: "fixed",
+                  height: "inherit",
+                  margin: "auto"
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: offsetX / offset + "px",
+                    marginTop: offsetY / offset + "px"
+                  }}
+                >
+                  {child}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Parallax;
